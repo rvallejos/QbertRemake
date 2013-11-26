7dc33e009075f714dc51f8dbd78e9c21970212c3
vitnguye <vitnguye@ucsc.edu>
2013-11-26 00:42:12 -0800

    Left and right movement

---
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/TestController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestController : MonoBehaviour {
+	public float movementSpeed = 5.0f;
+	private int jumpHeight = 500;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("a")){
+			transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+		}
+		if(Input.GetKey("d")){
+			transform.position -= Vector3.right * movementSpeed * Time.deltaTime;
+		}
+		if(Input.GetButtonDown("Jump")){
+			//Jump ();
+		}
+	}
+
+	void Jump(){
+		rigidbody.AddForce(new Vector3(0, jumpHeight, 0), ForceMode.Force);
+	}
+}
