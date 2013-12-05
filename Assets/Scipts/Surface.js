eee1e80779165679f9c6192cb48e722dbfcba88a
vitnguye <vitnguye@ucsc.edu>
2013-12-05 12:11:40 -0800

    Fixed color checking

--- Assets/Scipts/Surface.js
+++ Assets/Scipts/Surface.js
@@ -8,14 +8,14 @@ var increment : float = 0.03;
 var decrement : float = 0.00005;
 var radius : float = 3;
 var counter : int = 0;
-var startColor : Color = Color(0, 1.0, 0.25,1);
+var startColor : Color = Color(0, 1.0, 1.0,1);
 function Start () {
 
 }
 
 function OnCollisionEnter(collision:Collision)
 {
-    if( collision.gameObject.tag == "construction_worker" )
+    if( collision.gameObject.tag == "player" )
     {
         Destroy(collision.gameObject);
     }
@@ -25,26 +25,26 @@ function Update () {
 
 	var dist : float = Vector3.Distance(this.transform.position, player.position);
 
-		if (dist < radius) {
+	if (dist < radius) {
 
-			accumColor += increment;
-			}else{
-			accumColor -= decrement;
-		}
-	
-		if (accumColor > 1){
-			
-			accumColor = 1;
+		accumColor += increment;
+		}else{
+		accumColor -= decrement;
+	}
+
+	if (accumColor > 1){
 		
-		}
+		accumColor = 1;
 	
-		if (accumColor < 0){
-			
-			accumColor = 0;
-		}
+	}
+
+	if (accumColor < 0){
+		
+		accumColor = 0;
+	}
 	
 	
-	var myColor : Color = Color(0,accumColor + .5, accumColor - .75,1); //accumColor - .75
+	var myColor : Color = Color(0,accumColor, accumColor,1); //accumColor + .5 accumColor - .75
 	renderer.material.color = myColor;
 	if(startColor==myColor){
 		counter++;
@@ -53,6 +53,7 @@ function Update () {
 //		counter--;
 //	}
 	if(counter >= 21) Debug.Log("You Won!");
-	if(counter < 21) Debug.Log("Keep going!");
+	//if(counter < 21) 
+	else Debug.Log("Keep going");
 
 }
