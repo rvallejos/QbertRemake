99443edd2ee7b9751f449eff5aee25baea1830c2
Sean <seanstein88@gmail.com>
2013-12-05 00:01:29 -0800

    Added timer, End Game.

--- Assets/Scipts/UI.js
+++ Assets/Scipts/UI.js
@@ -1,13 +1,32 @@
 ﻿#pragma strict
 
+var timer:float = 60.0;
+var timeExp:boolean = false;
+
 function Start () {
 
 }
 
 function Update () {
-
+	if(timer <= 0){
+		timer = 0;
+		timeExp = true;
+		restartLevel();
+	} else {
+		timer -=Time.deltaTime;
+	}
 }
 
 function OnGUI(){
-GUI.Box(Rect(10,10,200,100),"Test GUI");
+	if(timeExp){
+		//Do Something Here
+		GUI.Label(Rect (10,10,150,30),"Time's Up! You lose!");
+	} else {
+		GUI.Label(Rect (10,10,100,30),"Time Left: " + timer.ToString("0")+"s");
+	}
+}
+
+function restartLevel(){
+	yield WaitForSeconds (2);
+	Application.LoadLevel(Application.loadedLevel);
 }
